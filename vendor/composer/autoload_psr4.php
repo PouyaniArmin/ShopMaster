b2b73635a18cf958d9bef81794f7a26456ed34f9
PouyaniArmin <pouyaniarmib@gmail.com>
2025-06-10 15:00:50 +0330

    add namesapce middleware

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -16,6 +16,7 @@ return array(
     'DeepCopy\\' => array($vendorDir . '/myclabs/deep-copy/src/DeepCopy'),
     'App\\Core\\ViewEngine\\' => array($baseDir . '/src/core/viewEngine'),
     'App\\Core\\Router\\' => array($baseDir . '/src/core/router'),
+    'App\\Core\\Middleware\\' => array($baseDir . '/src/core/middleware'),
     'App\\Core\\Database\\' => array($baseDir . '/src/core/database'),
     'App\\Core\\' => array($baseDir . '/src/core'),
     'App\\Controllers\\' => array($baseDir . '/src/controllers'),
